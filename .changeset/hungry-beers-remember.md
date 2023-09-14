5eb117aec314c62a1a5173da232744562940ea40
Putro <laustsf@gmail.com>
2023-09-14 21:48:28 +0200

    Provide type information for the prettier export

---
+++ .changeset/hungry-beers-remember.md
@@ -0,0 +1,5 @@
+---
+"@putstack/prettier-config": minor
+---
+
+Provide type information for the prettier config being exported
--- packages/eslint-config-typescript/tests/eslint-rule-tester.ts
+++
@@ -1,4 +0,0 @@
-import { RuleTester } from '@typescript-eslint/rule-tester';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ruleTester = new RuleTester();
