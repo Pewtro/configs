171c8c0d53a740d33b40e225bf2d26ce14a378e5
Putro <laustsf@gmail.com>
2023-12-14 21:49:21 +0100

    Add eslint-import-resolver-typescript and eslint-plugin-import and enable some configuration elements

--- packages/eslint-config-typescript/base/index.js
+++ packages/eslint-config-typescript/base/index.js
@@ -1,11 +1,17 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'progress', 'sonarjs'],
+  plugins: ['@typescript-eslint', 'progress', 'sonarjs', 'import'],
   extends: [
+    //General eslint recommended rules
     'eslint:recommended',
+    //General typescript-eslint rules that have type knowledge
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    //General code quality rules
     'plugin:sonarjs/recommended',
+    //General import rules
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   rules: {
     /** ESLint plugin configuration */
@@ -15,12 +21,27 @@ module.exports = {
     /** Stylistic rules */
     //Prefer using Array<T> over T[]
     '@typescript-eslint/array-type': ['warn', { default: 'generic', readonly: 'generic' }],
+    //We want to encourage marking type imports explicitly which is also enforced by TypeScripts --verbatimModuleSyntax
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    //We want to encourage marking type exports explicitly
+    '@typescript-eslint/consistent-type-exports': 'warn',
+    //Enforce the use of top-level import type qualifer when an import only has specifiers with inline type qualifiers
+    '@typescript-eslint/no-import-type-side-effects': 'warn',
+    //We want to encourage the import type specifiers to be consistent
+    'import/consistent-type-specifier-style': ['warn', 'prefer-top-level'],
+    //Set up a specificer import that we generally want to adhere to to make it easier recognizing where an import is coming from
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'unknown'],
+        'newlines-between': 'never',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
 
     /** Code quality rules */
     //Enforce default clauses in switch statements to be last
     'default-case-last': 'warn',
-    //Disallow duplicate imports
-    'no-duplicate-imports': 'warn',
     //Disallow nested ternary expressions
     'no-nested-ternary': 'warn',
 
@@ -38,5 +59,17 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    es6: true,
+  },
+  //Default settings to fit simple projects, but should be overridden by users who want/need more control
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
