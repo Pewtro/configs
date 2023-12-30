5480d429ba601251d088be27056deea78f8c9fc9
Putro <laustsf@gmail.com>
2023-12-30 20:11:36 +0100

    Add comment about the rule we turn off in this repo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     project: ['./tsconfig.json'],
   },
   rules: {
+    //Until we get to ESLint v9 with stable flat config, we need to turn this off
     'unicorn/prefer-module': 'off',
   },
 };
