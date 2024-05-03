f61c173964639b7f37bc3b0f0b4222c0634bf77f
Putro <29204244+Pewtro@users.noreply.github.com>
2024-05-03 18:54:00 +0200

    [Tests] Make tests run concurrently

--- packages/utils/src/http-utils/http-utils.test.ts
+++ packages/utils/src/http-utils/http-utils.test.ts
@@ -1,114 +1,120 @@
 /* eslint-disable sonarjs/no-duplicate-string */
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'vitest';
 import { generatePath } from './http-utils.js';
 
 const testingUrl = 'https://github.com';
 
-describe('generatePath with :', () => {
-  it('should maintain the prefix / when passed path starts with / and baseUrl does not exist', () => {
+describe.concurrent('generatePath with :', () => {
+  it('should maintain the prefix / when passed path starts with / and baseUrl does not exist', ({ expect }) => {
     const result = generatePath('/users/:userId/posts/:postId', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  it('removes ending / when passed path that ends with /', () => {
+  it('removes ending / when passed path that ends with /', ({ expect }) => {
     const result = generatePath('/users/:userId/posts/:postId/', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  it("doesn't add / at start when passed path doesn't start with /", () => {
+  it("doesn't add / at start when passed path doesn't start with /", ({ expect }) => {
     const result = generatePath('users/:userId/posts/:postId', { userId: '1', postId: '2' });
     expect(result).toBe('users/1/posts/2');
   });
-  it('keeps starting / and removes ending / when passed path starts with / and ends with /', () => {
+  it('keeps starting / and removes ending / when passed path starts with / and ends with /', ({ expect }) => {
     const result = generatePath('/users/:userId/posts/:postId/', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  describe('with baseurl', () => {
-    it('should maintain the prefix / when passed path starts with / and baseUrl does not end with /', () => {
+  describe.concurrent('with baseurl', () => {
+    it('should maintain the prefix / when passed path starts with / and baseUrl does not end with /', ({ expect }) => {
       const result = generatePath('/users/:userId/posts/:postId', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("keeps starting / and removes ending / when passed path starts with / and ends with / and baseUrl doesn't end with /", () => {
+    it("keeps starting / and removes ending / when passed path starts with / and ends with / and baseUrl doesn't end with /", ({
+      expect,
+    }) => {
       const result = generatePath('/users/:userId/posts/:postId/', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("removes ending / when passed path that ends with / and baseUrl doesn't end with /", () => {
+    it("removes ending / when passed path that ends with / and baseUrl doesn't end with /", ({ expect }) => {
       const result = generatePath('/users/:userId/posts/:postId/', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("doesn't add / at start when passed path doesn't start with / and baseUrl doesn't end with /", () => {
+    it("doesn't add / at start when passed path doesn't start with / and baseUrl doesn't end with /", ({ expect }) => {
       const result = generatePath('users/:userId/posts/:postId', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
   });
 });
 
-describe('generatePath with {}', () => {
-  it('should maintain the prefix / when passed path starts with / and baseUrl does not exist', () => {
+describe.concurrent('generatePath with {}', () => {
+  it('should maintain the prefix / when passed path starts with / and baseUrl does not exist', ({ expect }) => {
     const result = generatePath('/users/{userId}/posts/{postId}', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  it('removes ending / when passed path that ends with /', () => {
+  it('removes ending / when passed path that ends with /', ({ expect }) => {
     const result = generatePath('/users/{userId}/posts/{postId}/', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  it("doesn't add / at start when passed path doesn't start with /", () => {
+  it("doesn't add / at start when passed path doesn't start with /", ({ expect }) => {
     const result = generatePath('users/{userId}/posts/{postId}', { userId: '1', postId: '2' });
     expect(result).toBe('users/1/posts/2');
   });
-  it('keeps starting / and removes ending / when passed path starts with / and ends with /', () => {
+  it('keeps starting / and removes ending / when passed path starts with / and ends with /', ({ expect }) => {
     const result = generatePath('/users/{userId}/posts/{postId}/', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  describe('with baseurl', () => {
-    it('should maintain the prefix / when passed path starts with / and baseUrl does not end with /', () => {
+  describe.concurrent('with baseurl', () => {
+    it('should maintain the prefix / when passed path starts with / and baseUrl does not end with /', ({ expect }) => {
       const result = generatePath('/users/{userId}/posts/{postId}', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("keeps starting / and removes ending / when passed path starts with / and ends with / and baseUrl doesn't end with /", () => {
+    it("keeps starting / and removes ending / when passed path starts with / and ends with / and baseUrl doesn't end with /", ({
+      expect,
+    }) => {
       const result = generatePath('/users/{userId}/posts/{postId}/', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("removes ending / when passed path that ends with / and baseUrl doesn't end with /", () => {
+    it("removes ending / when passed path that ends with / and baseUrl doesn't end with /", ({ expect }) => {
       const result = generatePath('/users/{userId}/posts/{postId}/', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("doesn't add / at start when passed path doesn't start with / and baseUrl doesn't end with /", () => {
+    it("doesn't add / at start when passed path doesn't start with / and baseUrl doesn't end with /", ({ expect }) => {
       const result = generatePath('users/{userId}/posts/{postId}', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
   });
 });
 
-describe('generatePath with mixed : and {}', () => {
-  it('should maintain the prefix / when passed path starts with / and baseUrl does not exist', () => {
+describe.concurrent('generatePath with mixed : and {}', () => {
+  it('should maintain the prefix / when passed path starts with / and baseUrl does not exist', ({ expect }) => {
     const result = generatePath('/users/:userId/posts/{postId}', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  it('removes ending / when passed path that ends with /', () => {
+  it('removes ending / when passed path that ends with /', ({ expect }) => {
     const result = generatePath('/users/:userId/posts/{postId}/', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  it("doesn't add / at start when passed path doesn't start with /", () => {
+  it("doesn't add / at start when passed path doesn't start with /", ({ expect }) => {
     const result = generatePath('users/:userId/posts/{postId}', { userId: '1', postId: '2' });
     expect(result).toBe('users/1/posts/2');
   });
-  it('keeps starting / and removes ending / when passed path starts with / and ends with /', () => {
+  it('keeps starting / and removes ending / when passed path starts with / and ends with /', ({ expect }) => {
     const result = generatePath('/users/:userId/posts/{postId}/', { userId: '1', postId: '2' });
     expect(result).toBe('/users/1/posts/2');
   });
-  describe('with baseurl', () => {
-    it('should maintain the prefix / when passed path starts with / and baseUrl does not end with /', () => {
+  describe.concurrent('with baseurl', () => {
+    it('should maintain the prefix / when passed path starts with / and baseUrl does not end with /', ({ expect }) => {
       const result = generatePath('/users/:userId/posts/{postId}', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("keeps starting / and removes ending / when passed path starts with / and ends with / and baseUrl doesn't end with /", () => {
+    it("keeps starting / and removes ending / when passed path starts with / and ends with / and baseUrl doesn't end with /", ({
+      expect,
+    }) => {
       const result = generatePath('/users/:userId/posts/{postId}/', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("removes ending / when passed path that ends with / and baseUrl doesn't end with /", () => {
+    it("removes ending / when passed path that ends with / and baseUrl doesn't end with /", ({ expect }) => {
       const result = generatePath('/users/:userId/posts/{postId}/', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
-    it("doesn't add / at start when passed path doesn't start with / and baseUrl doesn't end with /", () => {
+    it("doesn't add / at start when passed path doesn't start with / and baseUrl doesn't end with /", ({ expect }) => {
       const result = generatePath('users/:userId/posts/{postId}', { userId: '1', postId: '2' }, testingUrl);
       expect(result).toBe(`${testingUrl}/users/1/posts/2`);
     });
