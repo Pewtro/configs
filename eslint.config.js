ff50d532364780497ae7c9fbc21c9010e2b765ae
Pewtro <29204244+Pewtro@users.noreply.github.com>
2025-05-09 14:24:36 +0200

    [Project] Remove turning off ban dependencies override in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -14,15 +14,4 @@ export default tseslint.config(
     },
   },
   putstack.configs.recommended,
-  {
-    //TODO: Remove this if/when we move to eslint-plugin-unicorn-x in a future major version of @putstack/eslint-config-typescript
-    rules: {
-      'depend/ban-dependencies': [
-        'error',
-        {
-          allowed: ['eslint-plugin-unicorn'],
-        },
-      ],
-    },
-  },
 );
