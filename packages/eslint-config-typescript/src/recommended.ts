49231bf5ee2b46e381f9f5f50a8b3f0d1ff443ee
Pewtro <29204244+Pewtro@users.noreply.github.com>
2025-06-14 12:07:14 +0200

    [ESLint] Temporary fix for issue in typescript-eslint 8.34.0

--- packages/eslint-config-typescript/src/recommended.ts
+++ packages/eslint-config-typescript/src/recommended.ts
@@ -2,6 +2,7 @@ import unicorn from 'eslint-plugin-unicorn';
 import { config } from 'typescript-eslint';
 import { base } from './base';
 
+//@ts-expect-error Temporary issue with typescript-eslint
 const unicornConfig = config(unicorn.configs.recommended);
 
 export const recommended = config(base, unicornConfig);
