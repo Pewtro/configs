8e9a73ac3f0fc9cfd299c19c7126fc27435236a1
Pewtro <29204244+Pewtro@users.noreply.github.com>
2025-06-18 15:47:22 +0200

    [Project] Update dev dependencies

--- packages/eslint-config-typescript/src/recommended.ts
+++ packages/eslint-config-typescript/src/recommended.ts
@@ -2,7 +2,6 @@ import unicorn from 'eslint-plugin-unicorn';
 import { config } from 'typescript-eslint';
 import { base } from './base';
 
-//@ts-expect-error Temporary issue with typescript-eslint
 const unicornConfig = config(unicorn.configs.recommended);
 
 export const recommended = config(base, unicornConfig);
