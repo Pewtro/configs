90152402f5f84edd0a8ebb473d110b83a32096c8
Pewtro <29204244+Pewtro@users.noreply.github.com>
2026-06-28 01:00:34 +0200

    [ESLint] Disable unicorn/consistent-class-member-order as it interferes with perfectionist

---
+++ .changeset/itchy-nails-try.md
@@ -0,0 +1,5 @@
+---
+'@putstack/eslint-config-typescript': patch
+---
+
+Disable unicorn/consistent-class-member-order as it interferes with perfectionist
--- packages/eslint-config-typescript/src/recommended.ts
+++ packages/eslint-config-typescript/src/recommended.ts
@@ -2,6 +2,10 @@ import unicorn from 'eslint-plugin-unicorn';
 import { defineConfig } from 'eslint/config';
 import { base } from './base';
 
-const unicornConfig = defineConfig(unicorn.configs.recommended);
+const unicornConfig = defineConfig(unicorn.configs.recommended, {
+  rules: {
+    'unicorn/consistent-class-member-order': 'off',
+  },
+});
 
 export const recommended = defineConfig(base, unicornConfig);
